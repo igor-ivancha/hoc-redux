3e4ec2f0f127344a4448adcb8a5d87a05917a7e4
igor-ivancha <sg.igor.iv@gmail.com>
2017-05-04 10:02:24 +0300

    add resources component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      Secret Recipe
+      <ul>
+        <li>Something big</li>
+        <li>And more big</li>
+        <li>And bigger</li>
+      </ul>
+    </div>
+  )
+}
